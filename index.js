6de79ee055f3c9f3ebb14fe5002c16eac42d1002
arandomgamer8 <51796146+arandomgamer8@users.noreply.github.com>
2022-05-21 18:20:17 +0100

    i cba to write mroe push messages

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const Discord = require('discord.js');
-require("dotenv").config();
+//requirments:
+const Discord = require('discord.js')
+require("dotenv").config()
 
+//intents:
 const client = new Discord.Client({
     intents: [
         'GUILDS',
         'GUILD_MESSAGES',
+        'GUILD_MEMBERS',
     ]
-});
-
-client.on('ready', () => {
-    console.log(`I am ready! Logged in as ${client.user.tag}`);
 })
 
-client.on("messageCreate", (message) => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-    }
-});
+let bot = {
+    client,
+    prefix: 'cd.',
+    owners: ['470249639845560320'],
+}
 
-client.login(process.env.TOKEN);
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+
+client.loadEvents = (bot, reload) => require('./handlers/events')(bot, reload)
+
+client.loadEvents(bot, false)
+
+module.exports = bot
+
+client.login(process.env.TOKEN)
 //Invite URL https://discord.com/api/oauth2/authorize?client_id=975168776700067910&permissions=412384349248&scope=bot%20applications.commands
